9ba2e4ab83206bbd823c675c2c83d49c1dcd7a91
Almaz Arayuly <almaztx@gmail.com>
2026-01-07 19:11:02 +0500

    Page Transition Animations with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { motion } from 'motion/react'
+import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router'
+import { AnimatePresence, motion } from 'motion/react'
 import AnimatedButton from './components/AnimatedButton'
 import { FeaturesList } from './components/FeaturesList'
 import { DraggableCard } from './components/DraggableCard'
@@ -11,8 +12,65 @@ import { LiveBadge } from './components/LiveBadge'
 function App() {
 
   return (
+    <BrowserRouter>
+      <div className='app'>
+        <nav className='nav'>
+          <div className='nav-links'>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+        </nav>
+        <AnimatedRoutes />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+function AnimatedRoutes() {
+  const location = useLocation()
+  return (
+    <AnimatePresence mode='wait'>
+      <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<PageTransition><HomePage /> </PageTransition>} />
+        <Route path='/about' element={<PageTransition><AboutPage /></PageTransition>} />
+        <Route path='/contact' element={<PageTransition><ContactPage /></PageTransition>} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
+
+function PageTransition({ children }) {
+  return <motion.main className='page'
+    initial={{ opacity: 0, x: 50 }}
+    animate={{ opacity: 1, x: 0 }}
+    exit={{ opacity: 0, x: -50 }}
+    transition={{ duration: 0.35, ease: "easeOut" }}
+  >
+    {children}</motion.main>
+}
+
+function HomePage() {
+  return (
     <>
-      <LiveBadge />
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, minima?</p>
+    </>
+  )
+}
+function AboutPage() {
+  return (
+    <>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, minima?</p>
+    </>
+  )
+}
+function ContactPage() {
+  return (
+    <>
+      <h2>Contact</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, minima?</p>
     </>
   )
 }
