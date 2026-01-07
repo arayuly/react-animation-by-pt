93bb3720194a3f1a4fb3c1cda77541f455b577b4
Almaz Arayuly <almaztx@gmail.com>
2026-01-07 18:28:02 +0500

    AnimatePresence

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { motion } from 'motion/react'
 import AnimatedButton from './components/AnimatedButton'
 import { FeaturesList } from './components/FeaturesList'
 import { DraggableCard } from './components/DraggableCard'
+import { DismissableAlert } from './components/DismissableAlert'
+import { StepSwitcher } from './components/StepSwitcher'
 
 function App() {
 
   return (
     <>
-      <DraggableCard />
+      <StepSwitcher />
     </>
   )
 }
